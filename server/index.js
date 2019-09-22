0653842a178cc630ca4a66f6e2987ecea18a0b9e
Jim Strother <jim.strother@gmail.com>
2019-09-21 17:29:07 -0700

    second commit

--- server/index.js
+++ server/index.js
@@ -11,6 +11,6 @@ app.use(cors());
 const posts = require('./routes/api/posts');
 app.use('/api/posts', posts);
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
